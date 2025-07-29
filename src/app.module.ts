baf08e1334cfdf115c4f8ec31d7599d4339f1226
varinder-singh10 <varinder.singh@joshtechnologygroup.com>
2025-07-29 23:17:40 +0530

    vs_ru_00: Used TypeormModule instead of Custom DatabaseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from 'app.controller';
 import { AppService } from 'app.service';
-import { DatabaseProvider } from '@common/database';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DEFAULT_DB_PORT } from '@constants/database.const';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseProvider],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT || '') || DEFAULT_DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: `${process.env.DB_PASSWORD}`,
+            database: process.env.DB_NAME,
+            autoLoadEntities: true,
+            synchronize: true,
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
