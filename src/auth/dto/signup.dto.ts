e078858d1d128b614db3a3fbb9144f3ccf24a618
varinder-singh10 <varinder.singh@joshtechnologygroup.com>
2025-07-30 18:34:30 +0530

    vs_ru_01: Add dto for user

---
+++ src/auth/dto/signup.dto.ts
@@ -0,0 +1,29 @@
+import {
+    IsEmail,
+    IsIn,
+    IsNotEmpty,
+    IsString,
+    IsStrongPassword,
+    MaxLength,
+    MinLength,
+} from 'class-validator';
+
+export class signupDto {
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(3)
+    @MaxLength(65)
+    name: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @IsStrongPassword()
+    password: string;
+
+    @IsNotEmpty()
+    @IsIn(['admin', 'seller', 'buyer'])
+    role: 'admin' | 'seller' | 'buyer';
+}
