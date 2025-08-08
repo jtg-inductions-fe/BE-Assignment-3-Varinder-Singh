4bbdb8c3ef56ae4b30f7d5a5d3bd43d4bb6d683e
varinder-singh10 <varinder.singh@joshtechnologygroup.com>
2025-08-08 16:08:28 +0530

    vs_ru_00: Review fixes

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,31 +1,39 @@
-import { INestApplication } from '@nestjs/common';
-import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, InternalServerErrorException } from '@nestjs/common';
 
 import * as request from 'supertest';
 import { App } from 'supertest/types';
 
-import { AppModule } from './../src/app.module';
+import { AppService } from './../src/app.service';
+import { createTestApp } from './createTestModule';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+  let appModule: INestApplication<App>;
+  let appService: AppService;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    const { app, module } = await createTestApp();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    appModule = app;
+    appService = module.get<AppService>(AppService);
   });
 
-  it('/GET /health', async () => {
-    return request(app.getHttpServer())
+  it('/GET /health should return 200 if app is running', async () => {
+    return request(appModule.getHttpServer())
       .get('/health')
       .expect(200)
       .expect({ message: 'app is running!' });
   });
 
+  it('/GET /health should return 500 if app is not running', async () => {
+    jest.spyOn(appService, 'healthCheck').mockImplementation(() => {
+      throw new InternalServerErrorException(
+        'An error occured while running app',
+      );
+    });
+    return request(appModule.getHttpServer()).get('/health').expect(500);
+  });
+
   afterAll(() => {
-    app.close();
+    appModule.close();
   });
 });
